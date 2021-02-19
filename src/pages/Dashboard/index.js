b76c33d1ff989bf6f8197d83b945c3aae9e8a734
Volodymyr Basok <volodymyrquo@gmail.com>
2021-02-19 18:22:04 +0200

    added  user profile component

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,12 +1,62 @@
-import React, { useState } from "react"
-import { Container } from "reactstrap"
+import React from "react"
+import { Container, Row, Col, Card, CardBody } from "reactstrap"
 
-const Dashboard = props => {
+//Import Breadcrumb
+
+import UserPhoto from "./UserPhoto"
+import UploadDocuments from "./UploadDocuments"
+
+const Dashboard = () => {
   return (
     <React.Fragment>
       <div className="page-content">
         <Container fluid>
-          <h1>Hello from Dashboard...</h1>
+          {/* Render Breadcrumb */}
+
+          <Row>
+            <Col xl="3">
+              <UserPhoto />
+            </Col>
+            <Col xl="3">
+              <UploadDocuments />
+            </Col>
+            <Col xl="6">
+              <Card>
+                <CardBody>
+                  <h1>user settings</h1>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            {" "}
+            <Col xl="6">
+              <Card>
+                <CardBody>
+                  <h1>contact settings</h1>
+                </CardBody>
+              </Card>
+            </Col>
+            <Col xl="6">
+              <Card>
+                <CardBody>
+                  <h1>requisites</h1>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
+
+          <Row>
+            {" "}
+            <Col xl="12">
+              <Card>
+                <CardBody>
+                  <h1>contacts table</h1>
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </Container>
       </div>
     </React.Fragment>
---
+++ src/store/cardsTypes/saga.ts
@@ -0,0 +1,29 @@
+import { call, put, takeEvery } from "redux-saga/effects"
+
+// Crypto Redux States
+import { GET_CARDS_TYPES } from "./actionTypes"
+import { getCardsTypesSuccess, getCardsTypesFail } from "./actions"
+
+//Include Both Helper File with needed methods
+import { getCardsTypes } from "../../common/data"
+
+//worker
+
+type GetCardsTypesTypes = ReturnType<typeof getCardsTypes>
+
+function* fetchCardsTypes() {
+  try {
+    const response:GetCardsTypesTypes = yield call(getCardsTypes)
+    yield put(getCardsTypesSuccess(response))
+  } catch (error) {
+    yield put(getCardsTypesFail(error))
+  }
+}
+
+//watcher
+
+function* getCardsTypesSaga() {
+  yield takeEvery(GET_CARDS_TYPES, fetchCardsTypes)
+}
+
+export default getCardsTypesSaga
